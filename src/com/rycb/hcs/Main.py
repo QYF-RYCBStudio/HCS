0b9ea284f5eacb2f674018f4ef2736f9bccf663a
QYF-RYCBStudio <74858895+QYF-RYCBStudio@users.noreply.github.com>
2022-02-11 20:05:01 +0800

    Add files via upload

--- src/com/rycb/hcs/Main.py
+++ src/com/rycb/hcs/Main.py
@@ -3,7 +3,7 @@
 # Create Time 2022/2/1 20:18
 # Location HCS-Python/Main.py
 # Use the PyCharm
-#
+# Encoding UTF-8
 # Do not change the code without special needs
 
 import configparser
@@ -28,13 +28,20 @@ class Main:
 
     @staticmethod  # 静态方法声明
     def progress_bar_error():
+        main.log("The progress bar module has been evoked.", "INFO", "Client")
         t.sleep(0.5)
         with tqdm(total=100) as p_bar:
+            main.log("The progress bar has been evoked.", "INFO", "Client")
             for i in range(99):
                 t.sleep(0.01)
                 p_bar.update(1)
                 p_bar.set_description("Try")
+                main.log("Loading resources: {}%".format(i), "INFO", "Client")
             print("Error: trying to reload...")
+            for j in range(100):
+                print("Repairing: {}%".format(i))
+                main.log("Error: Failed to loading resources, reloading: {}%".format(i), "ERROR", "Client")
+                t.sleep(0.025)
         main.__str__()
         main.progress_bar()
 
@@ -82,7 +89,9 @@ class Main:
 
     @staticmethod
     def startUp():
+        main.log("Loading resources...", "INFO", "Client")
         print("Program is loading resources...")
+        main.log("Evoke progress bar module", "INFO", "Client")
         main.progress_bar_error()
 
     @staticmethod
@@ -93,64 +102,67 @@ class Main:
 if __name__ == "__main__":
     with open("./logs/Runtime_" + Dt.strftime("%Y-%m-%d") + "_Log.log", "w") as file:
         file.write("")
-    Main().log("Program entry found.", "INFO", "CLIENT")
+    Main().log("Program entry found.", "INFO", "Client")
     main = Main()
-    main.log("Main class loading: successful.", "INFO", "CLIENT")
+    main.log("Main class loading: successful.", "INFO", "Client")
     eg.msgbox("若出现穿模、加载不全等问题，请停止程序后重启！\n\n若出現穿模、加載不全等問題，請停止程式後重啓！", "提示")
-    main.log("ShowTipBox showed: type number:001, state: successful.", "INFO", "CLIENT")
+    main.log("ShowTipBox showed: type number:001, state: successful.", "INFO", "Client")
     eg.msgbox("英语外其他语言可能有汉化不全等问题，请忽视或报告！\n\n英語外其他語言可能有漢化不全等問題，請忽視或報告！", "提示")
-    main.log("ShowTipBox showed: type number:002, state: successful.", "INFO", "CLIENT")
+    main.log("ShowTipBox showed: type number:002, state: successful.", "INFO", "Client")
     eg.codebox("（第一行）反馈&报告网址；（第二、三行）反馈&报告邮箱", "提示", "https://github.com/QYF-RYCBStudio/HCS/issues/new/choose"
                                                     "\nRYCBStudio@163.com\nRYCB_Feedback@163.com")
-    main.log("ShowIssueFeedbackBox showed: type number:003, state: successful.", "INFO", "CLIENT")
+    main.log("ShowIssueFeedbackBox showed: type number:003, state: successful.", "INFO", "Client")
     _return0 = None
-    main.log("Set variable: name: _return0, var number: 001, state: successful.", "INFO", "CLIENT")
+    main.log("Set variable: name: _return0, var number: 001, state: successful.", "INFO", "Client")
     choices = ["简体中文", "繁體中文", "English"]
     return0 = eg.choicebox("Choose your program language: (选择你的语言)", "rycbstudio.github.io", choices)
-    main.log("ShowChoiceBox showed: type number:004, state: successful.", "INFO", "CLIENT")
+    main.log("ShowChoiceBox showed: type number:004, state: successful.", "INFO", "Client")
     if return0 == "简体中文":
         _return0 = "zh-CN"
         main.log("Reassignment variable: name: _return0, Reassignment content: [string]'zh-CN', var number: 001, "
-                 "state: successful.", "INFO", "CLIENT")
+                 "state: successful.", "INFO", "Client")
     elif return0 == "繁體中文":
         _return0 = "zh-TW&zh-HK"
         main.log("Reassignment variable: name: _return0, Reassignment content: [string]'zh-TW&zh-HK', var number: 001, "
-                 "state: successful.", "INFO", "CLIENT")
+                 "state: successful.", "INFO", "Client")
     elif return0 == "English":
         _return0 = "en-US"
         main.log("Reassignment variable: name: _return0, Reassignment content: [string]'en-US', var number: 001, "
-                 "state: successful.", "INFO", "CLIENT")
+                 "state: successful.", "INFO", "Client")
     else:
         eg.msgbox("您已撤销操作！")
-        main.log("Warning: User has exited the program. return value: 0x5181. state: successful.", "WARN", "CLIENT")
+        main.log("Warning: User has exited the program. return value: 0x5181. state: successful.", "WARN", "Client")
         exit(0x5181)
 
-    main.log("Prepare to loading resources. progress: 0%", "INFO", "CLIENT")
+    main.log("Prepare to loading resources. progress: 0%", "INFO", "Client")
     t.sleep(0.25)
-    main.log("Prepare to loading resources. progress: 10%", "INFO", "CLIENT")
+    main.log("Prepare to loading resources. progress: 10%", "INFO", "Client")
     t.sleep(0.25)
-    main.log("Prepare to loading resources. progress: 25%", "INFO", "CLIENT")
+    main.log("Prepare to loading resources. progress: 25%", "INFO", "Client")
     t.sleep(0.25)
-    main.log("Prepare to loading resources. progress: 30%", "INFO", "CLIENT")
+    main.log("Prepare to loading resources. progress: 30%", "INFO", "Client")
     t.sleep(0.25)
-    main.log("Prepare to loading resources. progress: 45%", "INFO", "CLIENT")
+    main.log("Prepare to loading resources. progress: 45%", "INFO", "Client")
     t.sleep(0.25)
-    main.log("Prepare to loading resources. progress: 60%", "INFO", "CLIENT")
+    main.log("Prepare to loading resources. progress: 60%", "INFO", "Client")
     t.sleep(0.25)
-    main.log("Prepare to loading resources. progress: 75%", "INFO", "CLIENT")
+    main.log("Prepare to loading resources. progress: 75%", "INFO", "Client")
     t.sleep(0.25)
-    main.log("Prepare to loading resources. progress: 95%", "INFO", "CLIENT")
+    main.log("Prepare to loading resources. progress: 95%", "INFO", "Client")
     t.sleep(0.25)
-    main.log("Prepare to loading resources. progress: 100%", "INFO", "CLIENT")
-    main.log("Prepare to loading resources. state: successful.", "INFO", "CLIENT")
+    main.log("Prepare to loading resources. progress: 100%", "INFO", "Client")
+    main.log("Prepare to loading resources. state: successful.", "INFO", "Client")
     try:
         main.loading_resources("./lang", "{}.lang".format(_return0), "./license", "GPLv3.lcs", "./license", "MIT.lcs")
     except Exception as e:
-        main.log("Error: Some problems have been captured in some places. Please check:" + str(e), "ERROR", "CLIENT")
+        main.log("Error: Some problems have been captured in some places. Please check:" + str(e), "ERROR", "Client")
         eg.msgbox("Error: Some problems have been captured in some places. Please check:" + str(e))
     if _return0 == "zh-CN":
+        main.log("Has entered the simplified Chinese environment.", "INFO", "Client")
         print("语言包已成功加载。", end="\n")
+        main.log("Language pack loaded successfully.", "INFO", "Client")
         print("初始化中...", end="\n")
+        main.log("Initializing...", "INFO", "Client")
         try:
             main.startUp()
             print("程序初始化成功。", end="\n")
