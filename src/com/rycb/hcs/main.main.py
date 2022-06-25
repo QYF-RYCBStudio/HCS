87a6600446aaa30845615e57b4f133457224fe06
QYF-RYCBStudio <qyf090506@163.com>
2022-06-25 14:37:58 +0800

    Update main.main.py

--- src/com/rycb/hcs/main.main.py
+++ src/com/rycb/hcs/main.main.py
@@ -15,9 +15,34 @@ import util
 def main():
     choice = ["学生模式", "教师模式", "管理员模式"]
     eg_res = eg.choicebox("请选择模式：", "提示", choice, 0)
+    eg_res_name = ""
+    eg_res_pwd = ""
     if eg_res != choice[0]:
-        if eg_res == choice[1]:
-            util.multenterbox()
+        if eg_res == choice[2]:
+            eg_res_ = util.multenterbox(user_type="管理员")
+            if util.isAdmin(eg_res_[0]):
+                eg_res_name = eg_res_[0]
+                eg_res_pwd = eg_res_[1]
+                print(eg_res_name, eg_res_pwd)
+            else:
+                eg.msgbox("输入的管理员账户名称不合法", "警告")
+                exit(0xFFFFFF)
+        elif eg_res == choice[1]:
+            eg_res_ = util.multenterbox()
+            if util.isAdmin(eg_res_[0]):
+                eg_res_name = eg_res_[0]
+                eg_res_pwd = eg_res_[1]
+                print(eg_res_name, eg_res_pwd)
+            else:
+                eg.msgbox("输入的教师账户名称不合法", "警告")
+                exit(0xFFFFFF)
+        else:
+            exit(0xFFFFFF)
+    elif eg_res == choice[0]:
+        pass
+    else:
+        exit(0xFFFFFF)
+    util.saveInfo("["+eg_res_name+"|"+eg_res_pwd+"]", ".\\", "users.info")
 
 
 if __name__ == "__main__":
