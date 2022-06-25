bf92e13b2b0602e8d44d820ba4d5cdde143f92a0
QYF-RYCBStudio <qyf090506@163.com>
2022-06-25 14:18:29 +0800

    Create main.main.py

---
+++ src/com/rycb/hcs/main.main.py
@@ -0,0 +1,15 @@
+# Author RYCBStudio
+# License MIT
+# Create Time 2022/6/25 14:14
+# Location HCS/main.main.py
+# Use the PyCharm
+# Encoding UTF-8
+# Do not change the code without special needs
+#import sth
+import easygui as eg
+import util
+def main():
+    choice = ["学生模式", "教师模式", "管理员模式"]
+    eg_res = eg.choicebox("")
+if __name__ == "__main__":
+    main()
